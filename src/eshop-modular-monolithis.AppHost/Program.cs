53e9f565bb262a78406f0db8a99f878139d9b659
sachin kumar <sachindewan12@gmail.com>
2024-11-29 16:28:00 +0530

    Added keyclock aspire hosting package

--- src/eshop-modular-monolithis.AppHost/Program.cs
+++ src/eshop-modular-monolithis.AppHost/Program.cs
@@ -4,9 +4,15 @@ var password = builder.AddParameter("password", secret: true);
 
 var postgres = builder.AddPostgres("EshopServer", username, password).WithPgAdmin().WithDataVolume();
 var EshopDb = builder.ExecutionContext.IsRunMode ? postgres.AddDatabase("EshopDb") : builder.AddManagedResource("EshopDb");
+var keyclock = builder.AddKeycloak("identity");
 var redis = builder.ExecutionContext.IsPublishMode ? builder.AddRedis("redis").WithRedisInsight() : builder.AddManagedResource("redis");
+var rabbitMq = builder.ExecutionContext.IsPublishMode ? builder.AddRabbitMQ("messageBroker").WithManagementPlugin() : builder.AddManagedResource("messageBroker");
 builder.AddProject<Projects.Api>("api").WithReference(EshopDb)
     .WithReference(redis)
+    .WithReference(rabbitMq)
+    .WithReference(keyclock)
+    .WaitFor(rabbitMq)
+    .WaitFor(keyclock)
     .WaitFor(EshopDb)
     .WaitFor(redis);
 
